351b2e9fd477fcb2271f750b2c09d351fef99b24
crowdedpoem <tasschuerlein@yahoo.com>
2023-11-09 10:22:01 -0800

    updated schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,55 +1,25 @@
-const { PrismaClient } = require('@prisma/client');
+import { ayaan } from "./ayaan";
+import { sankalp } from "./sankalp";
+
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-    // clears the database
-    await prisma.user.deleteMany();
 
-    const sankalp = await prisma.user.create({
-        data: {
-            email: 'sankalp@gmail.com',
-            name: 'sankalp',
-            post: {
-                create: {
-                 
-                    positions: {
-                        create: [
-                            {
-                                startTime: new Date(),
-                                title: 'Position Title',
-                                description: 'Position Description',
-                                endTime: new Date(),
-                                pros: {
-                                    create: [
-                                        {
-                                            description: 'Pro Description',
-                                        },
-                                    ],
-                                },
-                                cons: {
-                                    create: [
-                                        {
-                                            description: 'Con Description',
-                                        },
-                                    ],
-                                },
-                            },
-                            // Add more positions as needed
-                        ],
-                    },
-                },
-            },
-        },
-    });
+  const resSan = await prisma.user.create({
+    data: sankalp,
+  });
+  const resAyaan = await prisma.user.create({
+    data: ayaan,
+  });
 }
 
-
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit();
-    });
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit();
+  });
