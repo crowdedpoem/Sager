1d503fb18262be70f7a8fbd617c641f17d396742
crowdedpoem <tasschuerlein@yahoo.com>
2023-11-20 13:17:25 -0800

    api call to add post

--- src/app/api/addPost/route.ts
+++ src/app/api/addPost/route.ts
@@ -32,7 +32,7 @@ export async function POST(request: Request) {
        }}
       ,
 
-      TimeLines: {
+      Experiences: {
         create: [
           {
             startDate: timeline.startDate,
