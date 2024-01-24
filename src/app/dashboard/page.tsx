20ebd504e878dce95c94b2763cd0ac7d53cda978
crowdedpoem <tasschuerlein@yahoo.com>
2024-01-24 15:58:51 -0800

    add key to posts on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ export default async function Page() {
                 <Suspense fallback={<Loading />}>
                     <div className="flex flex-wrap -mx-1 lg:-mx-4">
                         {experiences?.map((exp: any) => (
-                            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                            <div key={exp.userId} className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
                                 <article className="overflow-hidden rounded-lg shadow-lg bg-white">
                                     <Link href={{ pathname: `/dashboard/post/${exp.userId}`}}>
                                         <div className={`bg-purple block h-52 w-full`}>
