e869409c1ee4872259c16dd4ded57e7a85b2c7ab
Sankalp Varshney <sankalpvarshney12@gmail.com>
2024-02-15 12:00:00 -0800

    converted queryProvider to .tsx
    children prop is of type React.ReactNode

--- src/app/dashboard/queryProvider.tsx
+++ src/app/dashboard/queryProvider.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
-const QProvider = ({ children }) => {
+const QProvider = ({ children }: { children: React.ReactNode }) => {
     const [client] = useState(new QueryClient())
 
     return <QueryClientProvider client={client}>{children}</QueryClientProvider>
