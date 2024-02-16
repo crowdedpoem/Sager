51943643be25053ef8c1f72622737ab574e55110
crowdedpoem <tasschuerlein@yahoo.com>
2024-02-16 13:20:31 -0800

    fix ui errors

--- src/components/DayInTheLife.tsx
+++ src/components/DayInTheLife.tsx
@@ -14,7 +14,7 @@ export default function DayInTheLife(props: { items: any }) {
                             <details className="p-4 [&_svg]:open:-rotate-180" open>
                                 <summary className="flex cursor-pointer list-none items-center gap-4">
                                     <div>
-                                        <svg className="rotate-0 transform text-purple transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                                        <svg className="rotate-0 transform text-purple transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
                                             <polyline points="6 9 12 15 18 9"></polyline>
                                         </svg>
                                     </div>
